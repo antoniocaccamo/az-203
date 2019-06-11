ce86ffcaf5e37a9d48d40a4331565506d56868f1
Caccamo Antonio <cons.CaccamoAntonio@sia.eu>
2019-06-11 17:17:16 +0200

    ADD : Batch Service example

---
+++ batch-services/batch-dotnet-ffmpeg-tutorial/batch-dotnet-ffmpeg-tutorial/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace Cert.AZ203.Batch.Service
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics;
+    using System.IO;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.Batch;
+    using Microsoft.Azure.Batch.Auth;
+    using Microsoft.Azure.Batch.Common;
+    using Microsoft.WindowsAzure.Storage;
+    using Microsoft.WindowsAzure.Storage.Blob;
+
+
+    class Program
+    {
+        Environment.GetEnvironmentVariable("windir");
+
+        // Batch account credentials
+        private const string BatchAccountName = "";
+        private const string BatchAccountKey  = "";
+        private const string BatchAccountUrl  = "";
+
+        // Storage account credentials
+        private const string StorageAccountName = "";
+        private const string StorageAccountKey  = "";
+
+        // Pool and Job constants
+        private const string PoolId = "WinFFmpegPool";
+        private const int DedicatedNodeCount = 0;
+        private const int LowPriorityNodeCount = 5;
+        private const string PoolVMSize = "STANDARD_A1_v2";
+        private const string JobId = "WinFFmpegJob";
+
+        static void Main(string[] args)
+        {
+            if (String.IsNullOrEmpty(BatchAccountName) ||
+                String.IsNullOrEmpty(BatchAccountKey) ||
+                String.IsNullOrEmpty(BatchAccountUrl) ||
+                String.IsNullOrEmpty(StorageAccountName) ||
+                String.IsNullOrEmpty(StorageAccountKey))
+            {
+                throw new InvalidOperationException("One or more account credential strings have not been populated. Please ensure that your Batch and Storage account credentials have been specified.");
+            }
+
+
+
+
+        }
+    }
+}
